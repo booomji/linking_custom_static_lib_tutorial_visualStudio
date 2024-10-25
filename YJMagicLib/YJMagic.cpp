599fff04075694adaf96ea427a984deb685f0768
behram <batwrk@gmail.com>
2024-10-25 20:07:25 +0530

    c1 -  source files project for the Static Lib
    We create our static lib in this VS C++ project

---
+++ YJMagicLib/YJMagic.cpp
@@ -0,0 +1,15 @@
+// YJMagic.cpp
+
+#include "YJMagic.h"
+
+#include <stdexcept>
+
+using namespace std;
+
+namespace YJMagic
+{
+	double YJMagic::Add(double a, double b)
+	{
+		return a + b;
+	}
+}
---
+++ YJMagicLib/YJMagic.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// YJMagic.h
+
+namespace YJMagic
+{
+	class YJMagic
+	{
+	public:
+		// Returns a + b
+		static double Add(double a, double b);
+	};
+}
